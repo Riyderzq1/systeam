788e714e062e041591377193e7fca80ad6efadd3
Riyderzq1 <50485040+Riyderzq1@users.noreply.github.com>
2019-05-29 01:39:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -816,7 +816,7 @@ client.on('guildMemberAdd', member => {
                       
                                      .addField(' الـسيرفر', `${member.guild.name}`,true)
                                        
-     .setFooter("**SERVER NAME **")
+     .setFooter("**#TheH1TS CLAN **")
         .setTimestamp()
     
       channel.sendEmbed(embed);
@@ -1243,6 +1243,47 @@ client.on('message', message => {
     }
     });
     
+
+
+
+
+
+
+
+
+client.on('message', message => {
+
+    if(message.content === prefix + "$mutechannel") {
+                        if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+
+           }).then(() => {
+               message.reply("**__تم تقفيل الشات__ :white_check_mark: **")
+           });
+             }
+//FIRE BOT
+ if(message.content === prefix + "$unmutechannel") {
+                     if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+
+           }).then(() => {
+               message.reply("**__تم فتح الشات__:white_check_mark:**")
+           });
+             }
+             
+      
+    
+});
+
+
+
+
 
 
 
